80b8cc9ee5c21b8477b08ed5b8106c0ac75ffabb
Vaughn Dice <donvice@gmail.com>
2015-08-14 09:51:44 -0700

    Implement save instance method for Word class

--- lib/word.rb
+++ lib/word.rb
@@ -1,7 +1,7 @@
 class Word
   attr_reader(:self, :definitions)
   @@Words = []
-  
+
   define_method(:initialize) do |attributes|
     @self = attributes.fetch(:self)
     @definitions = []
@@ -9,6 +9,10 @@ class Word
 
   define_method(:add_definition) do |definition|
     @definitions.push(definition)
+  end
+
+  define_method(:save) do
+    @@Words.push(self)
   end
 
   define_singleton_method(:all) do
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -40,4 +40,11 @@ describe(Word) do
       expect(Word.all()).to(eq([]))
     end
   end
+
+  describe('#save') do
+    it('saves the word instance to the Word class array') do
+      @test_word.save()
+      expect(Word.all()).to(eq([@test_word]))
+    end
+  end
 end
