5cf99da60a1800211b0d8685b3573537f1a29f4f
Vaughn Dice <donvice@gmail.com>
2015-08-14 10:15:34 -0700

    Implement ability to post new word to index

--- app.rb
+++ app.rb
@@ -2,12 +2,32 @@ require('sinatra')
 require('sinatra/reloader')
 also_reload('lib/**/*.rb')
 require('./lib/word')
+require('./lib/definition')
 
 get('/') do
   @words = Word.all()
   erb(:index)
 end
 
+post('/') do
+  word_name = params.fetch('word_name')
+  word_definition = params.fetch('word_definition')
+
+  new_word = Word.new({:self => word_name})
+  new_definition = Definition.new({:self => word_definition,
+                                   :word => new_word})
+
+  new_word.add_definition(new_definition)
+  new_word.save()
+
+  @words = Word.all()
+  erb(:index)
+end
+
 get('/words/new') do
   erb(:new_word)
+end
+
+get('/words/:name') do
+  erb(:word)
 end
